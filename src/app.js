0659f0b47f071ead839f7669b48590a3ad22fd02
Tirumalesh Naik <159473514+tirumaleshnaik@users.noreply.github.com>
2025-01-17 11:54:30 +0530

    basics of module import and export

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+const calc = require("./calculator.js")
+
 const a = 5;
 const b= 10;
+calc(a,b);
 console.log("Hello World", + a+b);
+console.log(globalThis===global)
---
+++ src/calculator.js
@@ -0,0 +1,12 @@
+console.log("calculator");
+
+function sum(a,b){
+    console.log("sum is : ", a+b)
+    return a+b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+module.exports = sum;
